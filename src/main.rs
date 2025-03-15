8e5a68946b2c47ca76a38c809e220a1b5ebe1645
sangho007 <lk4820asdf@gmail.com>
2025-03-15 17:35:21 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// lane_detection 모듈 불러오기 (같은 crate 안에 있다고 가정)
+mod lane_detection;
+use lane_detection::{Pipeline,LaneDetectionResult};
+
+fn main() -> LaneDetectionResult<()> {
+    // 파이프라인 생성
+    let mut drive = Pipeline::new()?;
+    let _ = drive.start_detection(0);   // 0 : video , 1 : cam
+
+    Ok(())
+}
